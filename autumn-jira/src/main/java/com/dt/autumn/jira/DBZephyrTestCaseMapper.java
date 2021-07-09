3639a5d6e6bcf5808c6c7526acfb6bd8537932ed
Himanshu Aggarwal <himanshuaggarwal@himanshu.aggarwal>
2021-07-09 20:43:24 +0530

    Added apache license

--- autumn-jira/src/main/java/com/dt/autumn/jira/DBZephyrTestCaseMapper.java
+++ autumn-jira/src/main/java/com/dt/autumn/jira/DBZephyrTestCaseMapper.java
@@ -1,5 +1,25 @@
 package com.dt.autumn.jira;
 
+/*-
+ * #%L
+ * autumn-jira
+ * %%
+ * Copyright (C) 2021 Deutsche Telekom AG
+ * %%
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ * 
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ * 
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ * #L%
+ */
+
 import com.dt.autumn.jira.dataObjects.DBZephyrTestCaseDTO;
 import com.dt.autumn.utils.databaseUtils.MongoDBUtil;
 import com.dt.autumn.api.jsonProcessor.JacksonJsonImpl;
